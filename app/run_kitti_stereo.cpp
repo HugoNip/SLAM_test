1245d4ac4a210c8776a47b3b0741251b91f4cef1
Hugo Nie <gnie@ncsu.edu>
2020-06-25 23:14:37 -0400

    Add files via upload

--- app/run_kitti_stereo.cpp
+++ app/run_kitti_stereo.cpp
@@ -6,6 +6,7 @@ DEFINE_string(config_file, "../config/default.yaml", "config file path");
 int main(int argc, char** argv) {
     google::ParseCommandLineFlags(&argc, &argv, true);
 
+    std::cout << "yaml file locates in " << FLAGS_config_file << std::endl; // ../config/default.yaml
     myslam::VisualOdometry::Ptr vo(new myslam::VisualOdometry(FLAGS_config_file));
     assert(vo->Init() == true);
     vo->Run();
